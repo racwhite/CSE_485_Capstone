211c697ee899b5fb1e466909687bab6836231728
astefa10 <61570187+astefa10@users.noreply.github.com>
2020-11-06 10:52:57 -0800

    Update cactus.py

--- CSE_485_Programatic_Tracer5/CactusStack/cactus.py
+++ CSE_485_Programatic_Tracer5/CactusStack/cactus.py
@@ -1,4 +1,7 @@
 class Node:
+	# list of scopes
+	scopes = []
+	
 	def __init__(self, id, vars):
 		# frame name
 		self.id = id
@@ -8,11 +11,13 @@ class Node:
 		self.parent = None
 		# tracking children allows for backtracking to 'popped' frames
 		self.children = []
+		# add scope to list
+		Node.scopes.append(id)
 		# new node represents current frame
 		self.current_frame = self
 		# global scope has no enclosed scope, other scopes have enclosed scopes updated in push()
 		self.enclosed_frame = None
-
+		
 	# scope enter, add child node
 	def push(self, child):
 		# parent of new node is the enclosing scope(presently the current frame)
@@ -69,14 +74,17 @@ class Node:
 	# find var in current frame stack
 	def find_var(self, var):
 		temp = self.current_frame
-		# begin by looking at local vars, and work up to global
-		while(temp is not None):
-			if(var in temp.vars):
-				# print scope name and var value
-				print('Variable found in: ', temp.id)
-				return temp.vars.get(var)
-			temp = temp.parent
-		return 'Not found'
+		if not self.is_empty():
+			# begin by looking at local vars, and work up to global
+			while(temp is not None):
+				if(var in temp.vars):
+					# print scope name and var value
+					print('Variable found in: ', temp.id)
+					return temp
+				temp = temp.parent
+			print('Not found')
+		else:
+			print('Stack is empty')
 
 	# check if frame stack is empty
 	def is_empty(self):
@@ -85,58 +93,78 @@ class Node:
 		else:
 			return False
 
+	# new program, empty scope list
+	def reset_scopes(self):
+		Node.scopes = []
+
 	# return children of current_frame
 	def get_children(self):
 		return self.current_frame.children
 
+	# only modify variable in current scope, essentially redundant with mod_outside_scope
+	def mod_current_scope(self, var, val):
+		if not self.is_empty():
+			# check if var exists in current scope
+			if var in self.current_frame.vars:
+				# if it does, modify it
+				self.current_frame.vars[var] = val
+			else:
+				print('Var not found')
+		else:
+			print('Stack is empty')
+
+	# modify variables in any scope that exists in the tree
+	def mod_outside_scope(self, var, val, name):
+		# make sure scope exists
+		if name not in Node.scopes:
+			print('No scope called %s' % name)
+		else:
+			# check if current scope is the desired one
+			if self.id == name:
+				# if so, make sure variable exists in scope
+				if var in self.vars:
+					# if so, modify it
+					self.vars[var] = val
+				else:
+					print('No variable called %s in %s' % (var, name))
+			else:
+				# since scope exists in tree, recursively search for it
+				for child in self.children:
+					child.mod_outside_scope(var, val, name)
+
+	# print scopes that exist in tree
+	def print_all_scopes(self):
+		if not self.is_empty():
+			print(repr(self.scopes) + '\n')
+		else:
+			print('Stack is empty')
+
+	# print tree structure
+	def print_tree(self, level = 0):
+		# print scope name and vars
+		# indents represent child vs parent
+		ret = '\t'*level+self.id+repr(self.vars)+'\n'
+		# recursively traverse tree
+		for child in self.children:
+			# increase indent to represent children
+			ret += child.print_tree(level+1)
+		return ret
 
 
 if __name__ == '__main__':
-	Cactus = Node('__module__', {'func1':'func1', 'func2':'func2', 'fucn3':'func3', 'a':1, 'b':2, 'c':3, 'd':4})
+	Cactus = Node('__module__', {'func1':'func1', 'func2':'func2', 'fucn3':'func3', 'a':1, 'b':2, 'c':3, 'd':4, 'z':8})
 	Cactus.push(Node('func1', {'a':10, 'b':20, 'c':30}))
 	Cactus.push(Node('func2', {'a':100, 'b':200, 'c':300}))
-	Cactus.print_frame_stack()
-
-	print(Cactus.find_var('c'))
-	print('\n')
-
 	Cactus.pop()
-	print(Cactus.get_children()[0].id)
-	print(Cactus.get_children()[0].vars)
-	print('\n')
-
-	Cactus.print_frame_stack()
-	print('Children of current frame %s: ' % Cactus.current_frame.id)
-	for child in Cactus.get_children():
-		print(child.id)
-	print('\n')
-
 	Cactus.pop()
-	Cactus.print_frame_stack()
-
 	Cactus.push(Node('func3', {'a':1000, 'b':2000, 'c':3000}))
-	Cactus.print_frame()
 
-	Cactus.pop()
-	Cactus.print_frame()
+	print(Cactus.print_tree())
 
-	print(Cactus.find_var('e'))
-	print('\n')
+	Cactus.print_all_scopes()
 
-	if(Cactus.is_empty()):
-		print('Stack is empty')
-	else:
-		print('Stack is not empty')
-	print('Children of current frame %s: ' % Cactus.current_frame.id)
-	for child in Cactus.get_children():
-		print(child.id)
-	print('\n')
+	Cactus.mod_current_scope('a', 700000)
+	Cactus.mod_outside_scope('z', 50000, '__module__')
+	Cactus.mod_outside_scope('b', 0, 'func2')
 
-	Cactus.pop()
-	if(Cactus.is_empty()):
-		print('Stack is empty')
-	else:
-		print('Stack is not empty')
-
-	Cactus.pop()
-	Cactus.print_frame()
+	print(Cactus.print_tree())
